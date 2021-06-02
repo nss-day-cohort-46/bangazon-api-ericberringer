a9fe17e3063190237c438fbccae123fd27ffb7dc
ericberringer <eric.berringer@gmail.com>
2021-06-02 11:28:55 -0500

    report brings back all unpaid orders, the customer name, and order id.

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import favoritebyuser_list
+from .views import favoritebyuser_list, unpaidorders_list
 
 urlpatterns = [
     path('reports/userfavs', favoritebyuser_list),
+    path('reports/unpaidorders', unpaidorders_list),
 ]
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -1,2 +1,3 @@
 from .connection import Connection
 from .users.sellersbyuser import favoritebyuser_list
+from .orders.unpaidorders import unpaidorders_list

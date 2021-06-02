b52cee2d029d84d0e7cd9dc558f091a9a540a068
ericberringer <eric.berringer@gmail.com>
2021-06-02 14:42:41 -0500

    Report is showing vehicle models with their price if it is over , with product id.

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import favoritebyuser_list, unpaidorders_list, completedorders_list, inexpensiveproducts_list
+from .views import favoritebyuser_list, unpaidorders_list, completedorders_list, inexpensiveproducts_list, expensiveproducts_list
 
 urlpatterns = [
     path('reports/userfavs', favoritebyuser_list),
     path('reports/unpaidorders', unpaidorders_list),
     path('reports/completedorders', completedorders_list),
     path('reports/inexpensiveproducts', inexpensiveproducts_list),
+    path('reports/expensiveproducts', expensiveproducts_list),
 ]

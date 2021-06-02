752a2430697dcd34648d4f152d167da0762d85cc
ericberringer <eric.berringer@gmail.com>
2021-06-02 14:14:20 -0500

    Report shows the vehicle model, id number, and price if it is under .

--- bangazon server.session.sql
+++ bangazon server.session.sql
@@ -1,12 +1,5 @@
 SELECT
-    u.id user_id,
-    u.first_name || " " || u.last_name AS full_name,
-    c.phone_number,
-    f.customer_id,
-    f.seller_id
-FROM
-    bangazonapi_customer c
-JOIN
-    bangazonapi_favorite f ON f.seller_id = c.id
-JOIN 
-    auth_user u ON u.id = c.user_id
+    p.price product_price
+FROM bangazonapi_product p
+
+
--- bangazonreports/views/__init__.py
+++ bangazonreports/views/__init__.py
@@ -2,3 +2,4 @@ from .connection import Connection
 from .users.sellersbyuser import favoritebyuser_list
 from .orders.unpaidorders import unpaidorders_list
 from .orders.completedorders import completedorders_list
+from .products.inexpensiveproducts import inexpensiveproducts_list

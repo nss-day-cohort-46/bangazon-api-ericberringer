3808bb2d48f42b202f8a8b22c0a14c4f85a46307
ericberringer <eric.berringer@gmail.com>
2021-06-02 13:32:17 -0500

    report of completed order is printing with order id, total outstanding dollar amount, and payment method.

--- bangazonreports/urls.py
+++ bangazonreports/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import favoritebyuser_list, unpaidorders_list
+from .views import favoritebyuser_list, unpaidorders_list, completedorders_list
 
 urlpatterns = [
     path('reports/userfavs', favoritebyuser_list),
     path('reports/unpaidorders', unpaidorders_list),
+    path('reports/completedorders', completedorders_list),
 ]
---
+++ bangazonreports/views/orders/completedorders.py
@@ -0,0 +1,50 @@
+import sqlite3
+from django.shortcuts import render
+from bangazonreports.views import Connection
+
+def completedorders_list(request):
+
+    if request.method == 'GET':
+    # Connect to project database
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+                SELECT
+                    SUM(p.price) total_order_price,
+                    u.first_name || " " || u.last_name customer_name,
+                    o.id order_id,
+                    pay.merchant_name payment_method
+                FROM bangazonapi_order o
+                JOIN bangazonapi_orderproduct op ON op.order_id = o.id
+                JOIN bangazonapi_product p ON p.id = op.product_id
+                JOIN bangazonapi_customer c ON c.id = o.customer_id
+                JOIN bangazonapi_payment pay ON pay.id = o.payment_type_id
+                JOIN auth_user u ON u.id = c.user_id
+                WHERE o.payment_type_id IS NOT NULL
+            """)
+
+            dataset = db_cursor.fetchall()
+
+            completed_orders = {}
+
+            for row in dataset:
+
+                oid = row["order_id"]
+
+                completed_orders[oid] = {}
+                completed_orders[oid]["id"] = oid
+                completed_orders[oid]["customer_name"] = row["customer_name"]
+                completed_orders[oid]["total_order_price"] = row["total_order_price"]
+                completed_orders[oid]["payment_method"] = row["payment_method"]
+
+        list_of_completed_orders = completed_orders.values()
+
+        template = 'orders/list_of_completed_orders.html'
+        context = {
+            'completedorder_list': list_of_completed_orders
+        }
+
+
+        return render(request, template, context)
